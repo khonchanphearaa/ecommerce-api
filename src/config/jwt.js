91697af05b253e84b8e9a4ed604b5f426b08fcb6
khonchanphearaa007 <khonchanphearaa@gmail.com>
2025-12-25 21:47:28 +0700

    update JWT token expiration

--- src/config/jwt.js
+++ src/config/jwt.js
@@ -2,6 +2,6 @@ import jwt from "jsonwebtoken";
 
 export const generateToken = (userId) => {
   return jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "15m"
+    expiresIn: "30d" // valid for 30 days
   });
 };
