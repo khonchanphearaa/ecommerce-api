6ae3d3f1a4965fbac6cd3a0198ac2170f43a11ed
khonchanphearaa007 <khonchanphearaa@gmail.com>
2025-12-31 23:36:51 +0700

    update route payment

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { protect } from "../middlewares/auth.middleware.js";
-import { createPayment } from "../controllers/payment.controller.js";
+import { createBakongPayment, confirmBakongPayment, getKHQRImage } from "../controllers/payment.controller.js";
 
 const router = express.Router();
 
-router.use(protect); // user must be logged in
-router.post("/create", createPayment);
+/* publoc route */
+
+router.use(protect);
+router.post("/create",protect, createBakongPayment);
+router.post("/qr",protect, getKHQRImage); 
+router.post("/confirm", confirmBakongPayment);
 
 export default router;
