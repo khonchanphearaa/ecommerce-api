8e74d8a251800b0679c5702295d829d46fb09f02
khonchanphearaa007 <khonchanphearaa@gmail.com>
2025-12-31 08:13:19 +0700

    update: cofig db

--- src/config/jwt.js
+++ src/config/jwt.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
-export const generateToken = (userId) => {
-  return jwt.sign({ userId }, process.env.JWT_SECRET, {
+export const generateToken = (user) => {
+  return jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
     expiresIn: "30d" // valid for 30 days
   });
 };
