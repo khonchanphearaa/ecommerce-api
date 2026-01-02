a496dd9289aec2767701df83d66f59c82f8be762
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-01-02 12:38:25 +0700

    update role category

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -59,7 +59,7 @@ export const deleteCategory = async (req, res) => {
     const category = await Category.findById(req.params.id);
     if (!category) return res.status(404).json({ message: "Category not found" });
 
-    await category.remove();
+    await Category.deleteOne({_id: req.params.id})
     res.json({ message: "Category deleted" });
   } catch (error) {
     res.status(500).json({ message: "Server error" });
