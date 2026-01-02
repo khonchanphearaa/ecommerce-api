3afbf674045e6c9e43d61826e8765081b7a27f27
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-01-02 17:54:35 +0700

    change route is role admin

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { protect } from "../middlewares/auth.middleware.js";
+import { isAdmin } from "../middlewares/admin.middleware.js";
 import {
   createProduct,
   getProducts,
@@ -12,14 +13,14 @@ import { upload } from "../middlewares/upload.js"; // multer + Cloudinary storag
 
 const router = express.Router();
 
-// Public Routes
+/*  Public Routes (USER + ADMIN) */
 router.get("/", getProducts);
 router.get("/:id", getProductById);
 
-// Admin Protected Routes
+/* Admin Only Routes */
 // Upload multiple images (max 5) directly to Cloudinary
-router.post("/", protect, upload.array("images", 5), createProduct);
-router.put("/:id", protect, upload.array("images", 5), updateProduct);
-router.delete("/:id", protect, deleteProduct);
+router.post("/", protect, isAdmin, upload.array("images", 5), createProduct);
+router.put("/:id", protect, isAdmin, upload.array("images", 5), updateProduct);
+router.delete("/:id", protect, isAdmin, deleteProduct);
 
 export default router;
