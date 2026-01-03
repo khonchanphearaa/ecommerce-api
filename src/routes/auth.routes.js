36612aa0e4171b1d830c3058a16dd777d9b5b108
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-01-04 01:02:55 +0700

    updating: protect security of route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { register, login, logout } from "../controllers/auth.controller.js";
 import { protect } from "../middlewares/auth.middleware.js";
+import { registerValidator } from "../validators/auth.validator.js";
+import { validate } from "../middlewares/validate.js";
+import { registerLimiter, loginLimiter } from "../middlewares/authLimiter.js";
 
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", registerLimiter, registerValidator, validate, register);
+router.post("/login", loginLimiter, login);
 router.post("/logout", protect, logout);
 
 export default router;
