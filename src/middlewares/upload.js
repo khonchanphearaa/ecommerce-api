59e9c64f19ade1fb4b540b5c8bd7cbee8de90418
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-01-04 00:58:32 +0700

    update size pushing into cloudinary image

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -3,8 +3,9 @@ import { v2 as cloudinary } from "cloudinary";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import multer from "multer";
 
-dotenv.config(); // MUST be at the top
+dotenv.config(); 
 
+//Cloudinary configuration
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
@@ -16,6 +17,9 @@ const storage = new CloudinaryStorage({
   params: {
     folder: "ecommerce/products",
     allowed_formats: ["jpg", "jpeg", "png"],
+    transformation: [
+      { width: 500, height: 500, crop: "fill", gravity: "auto" },
+    ]
   },
 });
 
