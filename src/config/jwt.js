d8d82a0005860f3a7789c365ea87cdf206d76566
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-01-05 20:28:19 +0700

    update Token -update access & generate token

--- src/config/jwt.js
+++ src/config/jwt.js
@@ -1,7 +1,17 @@
 import jwt from "jsonwebtoken";
 
-export const generateToken = (userId) => {
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: "7d" // valid for 7 days
-  });
+export const generateAccessToken = (userId, role) => {
+  return jwt.sign(
+    { id: userId, role },
+    process.env.JWT_ACCESS_SECRET,
+    { expiresIn: "15m" }
+  );
+};
+
+export const generateRefreshToken = (userId) => {
+  return jwt.sign(
+    { id: userId },
+    process.env.JWT_REFRESH_SECRET,
+    { expiresIn: "7d" }
+  );
 };
