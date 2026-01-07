e425427972ebf724833e75fea8046bb10796f2e0
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-01-07 23:23:00 +0700

    Change validate exprire test7d

--- src/config/jwt.js
+++ src/config/jwt.js
@@ -4,7 +4,7 @@ export const generateAccessToken = (userId, role) => {
   return jwt.sign(
     { id: userId, role },
     process.env.JWT_ACCESS_SECRET,
-    { expiresIn: "15m" }
+    { expiresIn: "1d" }
   );
 };
 
