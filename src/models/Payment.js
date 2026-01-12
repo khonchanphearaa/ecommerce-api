f5e0905b862a80ea7fb6d72b6529c37fcc5bae10
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-01-12 21:06:38 +0700

    add schema payment

--- src/models/Payment.js
+++ src/models/Payment.js
@@ -1,52 +1,10 @@
-// src/models/Payment.js
 import mongoose from "mongoose";
 
-const paymentSchema = new mongoose.Schema(
-  {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-
-    order: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Order",
-      required: true,
-    },
-
-    amount: {
-      type: Number,
-      required: true,
-    },
-
-    currency: {
-      type: String,
-      default: "KHR",   /* Cambodia Reil */
-    },
-
-    paymentMethod: {
-      type: String,
-      enum: ["BAKONG_KHQR", "CASH"],
-      default: "BAKONG_KHQR",
-    },
-
-    /* Generated QR data */
-    khqrString:{type: String},
-
-    /* Optional: customer/bank transcation reference */
-    transactionRef: {type: String},
-    stripePaymentIntentId: {
-      type: String,
-    },
-
-    status: {
-      type: String,
-      enum: ["PENDING", "PAID", "FAILED"],
-      default: "PENDING",
-    },
-  },
-  { timestamps: true }
-);
+const paymentSchema = new mongoose.Schema({
+  orderId: String,
+  amount: Number,
+  method: String,
+  status: String
+}, { timestamps: true });
 
 export default mongoose.model("Payment", paymentSchema);
